fe6b8330fee8926c6a8178cbaa4f1e4abfae92aa
vrndarani <98180585+vrndarani@users.noreply.github.com>
2022-06-19 18:19:43 +0530

    hometask

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hel lo world!");
+
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Empoyee's name for report filing is " + fullName);
+
+
+        String fullNameUp = fullName.toUpperCase();
+        System.out.println("NAME: " + fullNameUp);
+
+
+        fullName = "Иванов Семён Семёнович";
+        String fullNameReplace = fullName.replace("ё", "е");
+        System.out.println("Employee's name data: " + fullNameReplace);
     }
 }
